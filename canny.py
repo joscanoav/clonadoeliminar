a6b261e9f06dd0364a1eba88b9187d494679db1a
joscanoav <joscanoav@gmail.com>
2023-12-01 13:41:16 +0100

    python

--- canny.py
+++ canny.py
@@ -1,28 +1,30 @@
-#Bibliotecas
 import cv2
-img =import cv2
 
-# Inicializar la cámara
-cap = cv2.VideoCapture(0)
+def capturar_video():
+    # Inicializar la captura de video desde la cámara (0 indica la cámara predeterminada)
+    cap = cv2.VideoCapture(0)
 
-# Comprobar si la cámara se ha iniciado correctamente
-if not cap.isOpened():
-    print("Error al abrir la cámara.")
-    exit()
+    # Comprobar si la cámara se ha iniciado correctamente
+    if not cap.isOpened():
+        print("Error al abrir la cámara.")
+        exit()
 
-# Bucle principal
-while True:
-    # Leer un fotograma de la cámara
-    ret, frame = cap.read()
+    # Bucle principal para capturar video
+    while True:
+        # Leer un fotograma de la cámara
+        ret, frame = cap.read()
 
-    # Mostrar el fotograma en una ventana
-    cv2.imshow('Cámara Web', frame)
+        # Mostrar el fotograma en una ventana
+        cv2.imshow('Captura de Video', frame)
 
-    # Salir del bucle si se presiona la tecla 'q'
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        # Salir del bucle si se presiona la tecla 'q'
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-# Liberar la cámara y cerrar la ventana
-cap.release()
-cv2.destroyAllWindows()
+    # Liberar la cámara y cerrar la ventana
+    cap.release()
+    cv2.destroyAllWindows()
 
+if __name__ == "__main__":
+    # Llamar a la función para capturar video desde la cámara
+    capturar_video()
